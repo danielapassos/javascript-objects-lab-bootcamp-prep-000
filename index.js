3d1fe85fc065d19df88811256ae8bbdad7d47f25
Daniela Passos <moitidani@gmail.com>
2018-07-05 22:16:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,39 @@
+var recipes = {}
+
+/*
+ describe('updateObjectWithKeyAndValue(object, key, value)', function() {
+    it('returns an object with the orignal key value pairs and the new key value pair', function() {
+      var obj = { prop: 1 }
+
+      expect(updateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+*/
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  object [key] = value;
+  
+  return object;
+}
+
+/*
+describe('destructivelyUpdateObjectWithKeyAndValue(object, key, value)', function() {
+    it('updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object', function() {
+      var obj = { prop: 1 }
+
+      expect(destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', 2)).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+
+      expect(obj).toMatch({
+        prop: 1,
+        prop2: 2
+      })
+    })
+  })
+*/
+
+object [key] = value;
